6851da48df17c32d560866f80acc15cf52ecf1ec
Lakindu Nethsara <lakindunethsara378@gmail.com>
2025-04-03 11:49:41 +0530

    Update main.dart

--- lib/Task 01/main.dart
+++ lib/Task 01/main.dart
@@ -1,9 +1,42 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'User Registration Form',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: UserRegistrationForm(),
+    );
+  }
+}
+
 class UserRegistrationForm extends StatefulWidget {
   @override
   _UserRegistrationFormState createState() => _UserRegistrationFormState();
 }
 
-class
+class _UserRegistrationFormState extends State<UserRegistrationForm> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _confirmPasswordController =
+      TextEditingController();
 
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Registration Successful!')),
+      );
+      print('Name: ${_nameController.text}');
+      print('Email: ${_emailController.text}');
+    }
+  }
+}
